06ad12b8dd9c914bf49fe1f6dce641615a46119c
Mihail pital <mihail.pital@yandex.ru>
2016-11-24 23:04:27 +0300

    Migrated to Leaflet class system
    Started to implement for points

--- src/LabelItem.js
+++ src/LabelItem.js
@@ -4,6 +4,130 @@ modlue to create labelItems convenient for labelling and calculation
 
 var geomEssentials = require('./geomEssentials.js');
 
+var minTolerativeDistancePx = 5; //const
+
+var layerType = function(layer){
+  return (layer instanceof L.Polyline)?1:
+         (layer instanceof L.Polygon)?2:
+         0; //Marker || CircleMarker
+}
+
+var BasicMixin = {
+  data:[],
+  text:'',
+  style:'',
+  txSize:0,
+  layer:null,
+  host:null,
+  _itemPoly:false, //all available textlabel positions for this label comin in 1 polygon
+  ignoreWhileLabel:false,
+  isDegenerate:false,
+  index:function(){
+    return this.host.lastIndexOf(this);
+  },
+  layer_type:function(){
+    //TOFIX for polygon
+    if(!this._layer_type)this._layer_type = layerType(this.layer);
+    return this._layer_type;
+  },
+
+  /**
+  get all available positions for this item. Depending on layer_type -> diff funcs.
+  Used in clustering computation
+  */
+  getItemPoly:function(){
+    if(!this._itemPoly){
+      this._itemPoly =  this._getBoundary();
+    }
+    return this._itemPoly;
+  },
+  initializeBase:function(options){
+    this.text=options.text; this.style = options.style; this.layer = options.layer; this.host = options.hostArray; this.txSize = options.txSize;
+  }
+}
+
+var PointItem = L.Class.extend({
+  initialize:function(options){
+    this.initializeBase(options);
+    this.isDegenerate = true;
+  },
+
+  _getBoundary: function(){
+    return false; //TODO
+  },
+
+  applyFeatureData:function(){
+    //TODO
+  },
+
+  readData:function(){
+
+  }
+})
+
+PointItem.include(BasicMixin);
+
+var LineItem = L.Class.extend({
+  initialize:function(options){
+    this.initializeBase(options)
+    this.computed_lengths=[];
+    this.totalLength=0;
+    if(this.layer._parts.length===0)this.isDegenerate = true;
+  },
+
+  _getBoundary: function(){
+    return geomEssentials.computeLineBoundaryPolygon(this.data,this.txSize.y);
+  },
+
+  /**
+  Calculates total length for this polyline on screen, and lengths of each segments with their angles
+  @param {labelItem} item: an item to get above data to
+  */
+  applyFeatureData:function(){
+    this.totalLength=0;
+    this.computed_lengths = geomEssentials.computeSegmentsLengths(this.data);
+    for(var k=0;k<this.computed_lengths.length;k++){
+      this.totalLength+=this.computed_lengths[k];
+    }
+    this.ignoreWhileLabel=this.totalLength<this.txSize.x;
+  },
+
+  //orig part
+  readData:function(partIndex){ //to read consequently
+    if(!partIndex){var partIndex=0;};
+    var nextPart = partIndex;
+    this.data = this.layer._parts[partIndex];
+    this.partIndex=partIndex; //store this to have ability to compute totalOffset, for example
+    //while(nextPart<this.layer._parts.length){
+    // NEEDS TO BE FINISHED
+    var nextPart=partIndex+1;
+    if(nextPart<this.layer._parts.length){
+      var notClonedNow=true;
+      while((this.layer._parts[partIndex][this.layer._parts[partIndex].length-1].distanceTo(this.layer._parts[nextPart][0])<minTolerativeDistancePx)
+             &&(nextPart+1<this.layer._parts.length)){
+        if(notClonedNow)this.data = this.layer._parts[partIndex].slice(0);
+        Array.prototype.push.apply(this.data, this.layer._parts[nextPart].slice(0));
+        partIndex++;
+        nextPart++;
+      }
+      return nextPart;
+    }else return false;
+  },
+
+  segCount:function(){return this.data.length -1},
+
+  /**
+  Get a segment from polyline part by it's offset
+  @param {Number} offset: na offset for the polyline
+  @returns {Array} : index of start point of segment and dist which is offset from start of the line to the end of found segment
+  */
+  getSegmentIdxAndDistByOffset:function(offset){
+    return geomEssentials.getSegmentIdxAndDistByOffset(offset,this.data,this.computed_lengths);
+  }
+});
+
+LineItem.include(BasicMixin);
+
 module.exports = {
   /**
   a factory function for label items
@@ -13,108 +137,18 @@ module.exports = {
   @param {L.Layer} layer: a feature (Marker, Polyline, Path) to aquire data
   */
   labelItem:function(text,style,txSize,layer,hostArray){
-
-    var minTolerativeDistancePx = 5; //const
-
-    var basic_item= {
-      data:[],
-      text:text,
-      style:style,
-      txSize:txSize,
-      layer:layer,
-      host:hostArray,
-      _itemPoly:false, //all available textlabel positions for this label comin in 1 polygon
-      ignoreWhileLabel:false,
-      index:function(){
-        return this.host.lastIndexOf(this);
-      },
-      readData:function(){return false}, //a method stub,
-      layer_type:function(){
-        //TOFIX for polygon
-        if(!this._layer_type)this._layer_type = (this.layer._parts.length>0)?1:0;
-        return this._layer_type;
-      },
-
-      _getBoundary:function(){return false;}, //a method stub, to obtain polygon with all postions
-      applyFeatureData:function(){}, //a method stub
-
-      /**
-      get all available positions for this item. Depending on layer_type -> diff funcs.
-      Used in clustering computation
-      */
-      getItemPoly:function(){
-        if(!this._itemPoly){
-          this._itemPoly =  this._getBoundary();
-        }
-        return this._itemPoly;
-      }
-    };
-
-    //TODO add prototyping || classes to handle differenet approaches for pt, poly and lines
-    //Not a very proper way to do such deal
-    if(basic_item.layer_type()==0){
-      return;
-      basic_item.readData = function(){
-        if(basic_item.layer.getLatLngs())
-        basic_item.data=basic_item.layer._map.latLngToLayerPoint(basic_item.layer.getLatLngs()[0]); //so we adding only L.Point obj
-      }
-    }else{
-      if(basic_item.layer._parts.length==0)return;
-      basic_item.computed_lengths=[];
-      basic_item.totalLength=0;
-      //this give possibility to read all parts to separate items
-      basic_item.readData=function(partIndex){ //to read consequently
-        if(!partIndex){var partIndex=0;};
-        var nextPart = partIndex;
-        this.data = this.layer._parts[partIndex];
-        this.partIndex=partIndex; //store this to have ability to compute totalOffset, for example
-        //while(nextPart<this.layer._parts.length){
-        // NEEDS TO BE FINISHED
-        var nextPart=partIndex+1;
-        if(nextPart<this.layer._parts.length){
-          var notClonedNow=true;
-          while((this.layer._parts[partIndex][this.layer._parts[partIndex].length-1].distanceTo(this.layer._parts[nextPart][0])<minTolerativeDistancePx)
-                 &&(nextPart+1<this.layer._parts.length)){
-            if(notClonedNow)this.data = this.layer._parts[partIndex].slice(0);
-            Array.prototype.push.apply(this.data, this.layer._parts[nextPart].slice(0));
-            partIndex++;
-            nextPart++;
-          }
-          return nextPart;
-        }else return false;
-      }
-
-      basic_item.segCount = function(){return this.data.length -1};
-
-      /**
-      Get a segment from polyline part by it's offset
-      @param {Number} offset: na offset for the polyline
-      @returns {Array} : index of start point of segment and dist which is offset from start of the line to the end of found segment
-      */
-      basic_item.getSegmentIdxAndDistByOffset=function(offset){
-        return geomEssentials.getSegmentIdxAndDistByOffset(offset,this.data,this.computed_lengths);
-      }
-
-      basic_item._getBoundary = function(){
-        return geomEssentials.computeLineBoundaryPolygon(this.data,this.txSize.y);
-      }
-
-      /**
-      Calculates total length for this polyline on screen, and lengths of each segments with their angles
-      @param {labelItem} item: an item to get above data to
-      */
-      basic_item.applyFeatureData=function(){
-        this.totalLength=0;
-        this.computed_lengths = geomEssentials.computeSegmentsLengths(this.data);
-        for(var k=0;k<this.computed_lengths.length;k++){
-          this.totalLength+=this.computed_lengths[k];
-        }
-        this.ignoreWhileLabel=this.totalLength<this.txSize.x;
-      }
-
+    var ltype= layerType(layer);
+    var opts = {text:text,style:style,txSize:txSize,layer:layer,hostArray:hostArray};
+    var result=false;
+    switch (ltype) {
+      case 0:
+        result = new PointItem(opts);
+        break;
+      case 1:
+        result= new LineItem(opts);
+        break;
     }
-
-    return basic_item;
+    return (!result.isDegenerate)?result:false;
   },
 
   candidatePosition:function(offset_or_origin,item){
